7a362cdf259928fcaa46c2252d88927ebd0f61c9
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-23 15:34:52 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="w-75">
+    <div class="d-flex">
+      <img src="../assets/img/dc-logo.png" alt="" srcset="">
+      <div id="headerSection" class="d-flex">
+        <a class="text-dark text-decoration-none" href="#">CHARACTERS</a>
+        <a class="text-dark text-decoration-none" href="#">COMICS</a>
+        <a class="text-dark text-decoration-none" href="#">MOVIES</a>
+        <a class="text-dark text-decoration-none" href="#">TV</a>
+        <a class="text-dark text-decoration-none" href="#">GAMES</a>
+        <a class="text-dark text-decoration-none" href="#">COLLECTIBLES</a>
+        <a class="text-dark text-decoration-none" href="#">VIDEOS</a>
+        <a class="text-dark text-decoration-none" href="#">FANS</a>
+        <a class="text-dark text-decoration-none" href="#">NEWS</a>
+        <a class="text-dark text-decoration-none" href="#">SHOP</a>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+#headerSection *{
+  margin: 0 0.5rem;
+}
+</style>
