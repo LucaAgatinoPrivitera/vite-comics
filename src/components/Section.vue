3ae81f70ccb40a8bd0f3a04c47ee7910738c1bf0
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-24 10:10:04 +0200

    Footer signup ho sistemato le icone

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -5,29 +5,29 @@
 <template>
     <div class="bg-primary" id="sectionHeight">
         <div class="w-75 m-auto py-4">
-            <div class="d-flex">
+            <div class="d-flex justify-content-center">
                 <div class="d-flex align-items-center justify-content-center mx-5">
-                    <img src="../assets/img/buy-comics-digital-comics.png" alt="" class="img-fluid w-25">
+                    <img src="../assets/img/buy-comics-digital-comics.png" alt="" class="img-fluid ">
                     <span class="text-light">DIGITAL COMICS</span>
                 </div>
 
                 <div class="d-flex align-items-center justify-content-center mx-5">
-                    <img src="../assets/img/buy-comics-merchandise.png" alt="" class="img-fluid w-25">
+                    <img src="../assets/img/buy-comics-merchandise.png" alt="" class="img-fluid ">
                     <span class="text-light">DC MERCHANDISE</span>
                 </div>
 
                 <div class="d-flex align-items-center justify-content-center mx-5">
-                    <img src="../assets/img/buy-comics-shop-locator.png" alt="" class="img-fluid w-25">
+                    <img src="../assets/img/buy-comics-shop-locator.png" alt="" class="img-fluid ">
                     <span class="text-light">SUBSCRIPTION</span>
                 </div>
 
                 <div class="d-flex align-items-center justify-content-center mx-5">
-                    <img src="../assets/img/buy-comics-subscriptions.png" alt="" class="img-fluid w-25">
+                    <img src="../assets/img/buy-comics-subscriptions.png" alt="" class="img-fluid ">
                     <span class="text-light">COMIC SHOP LOCATOR</span>
                 </div>
 
                 <div class="d-flex align-items-center justify-content-center mx-5">
-                    <img src="../assets/img/buy-dc-power-visa.svg" alt="" class="img-fluid w-25">
+                    <img src="../assets/img/buy-dc-power-visa.svg" alt="" class="img-fluid ">
                     <span class="text-light">DC POWER VISA</span>
                 </div>
                 <!-- Da chiedere perché mi da problemi che non rispetta il container anche con img fluid -->
@@ -41,7 +41,9 @@
 
 img{
     margin-right: 0.3rem;
-    width: 50px !important;
+    height: 50px;
+    width: 50px;
+    object-fit: contain;
 }
 span{
     margin-left: 0.3rem;
